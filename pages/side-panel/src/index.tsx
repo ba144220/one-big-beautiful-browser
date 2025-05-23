92d1a41d127a8ff07163567f7cc0ef5856e60e95
ba144220 <hs41511@gmail.com>
2025-05-23 12:39:12 -0700

    feat: email clerk auth setup

--- pages/side-panel/src/index.tsx
+++ pages/side-panel/src/index.tsx
@@ -1,6 +1,15 @@
 import { createRoot } from 'react-dom/client';
 import '@src/index.css';
 import SidePanel from '@src/SidePanel';
+import { ClerkProvider } from '@clerk/chrome-extension';
+
+const PUBLISHABLE_KEY = process.env.CEB_CLERK_PUBLISHABLE_KEY;
+const EXTENSION_URL = chrome.runtime.getURL('.');
+console.log(EXTENSION_URL);
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file');
+}
 
 function init() {
   const appContainer = document.querySelector('#app-container');
@@ -8,7 +17,15 @@ function init() {
     throw new Error('Can not find #app-container');
   }
   const root = createRoot(appContainer);
-  root.render(<SidePanel />);
+  root.render(
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY as string}
+      afterSignOutUrl={`${EXTENSION_URL}side-panel/index.html`}
+      signInFallbackRedirectUrl={`${EXTENSION_URL}side-panel/index.html`}
+      signUpFallbackRedirectUrl={`${EXTENSION_URL}side-panel/index.html`}>
+      <SidePanel />
+    </ClerkProvider>,
+  );
 }
 
 init();
