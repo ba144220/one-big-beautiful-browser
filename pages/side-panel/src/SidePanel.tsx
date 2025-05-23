dac3af69cbab7e28ddd6ba2911cfb5a79a48c5d7
ba144220 <hs41511@gmail.com>
2025-05-23 13:09:42 -0700

    feat: auth ui

--- pages/side-panel/src/SidePanel.tsx
+++ pages/side-panel/src/SidePanel.tsx
@@ -1,29 +1,18 @@
 import '@src/SidePanel.css';
 import { withErrorBoundary, withSuspense } from '@extension/shared';
-import { SignedIn, SignedOut, UserButton } from '@clerk/chrome-extension';
-import { Button } from '@src/components/ui/button';
-
+import { SignedIn, SignedOut } from '@clerk/chrome-extension';
+import Chatroom from './pages/Chatroom';
+import Signin from './pages/Signin';
 const SidePanel = () => {
-  const handleSignIn = async () => {
-    // Create a new tab
-    chrome.tabs.create({ url: process.env.CEB_WEB_URL as string });
-    // Close the side panel
-    window.close();
-  };
   return (
-    <div className="p-12">
-      <nav className="">
-        <SignedOut>
-          <Button size="sm" onClick={handleSignIn}>
-            Sign in
-          </Button>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </nav>
-      <Button>Click me</Button>
-    </div>
+    <>
+      <SignedOut>
+        <Signin />
+      </SignedOut>
+      <SignedIn>
+        <Chatroom />
+      </SignedIn>
+    </>
   );
 };
 
