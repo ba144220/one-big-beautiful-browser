f3f35bff69d998285e7e3b067e74620106acb166
ba144220 <hs41511@gmail.com>
2025-05-23 12:59:29 -0700

    setup: shadcn for web app and side panel

--- apps/web/src/components/test.tsx
+++ apps/web/src/components/test.tsx
@@ -1,3 +1,10 @@
+import { Button } from './ui/button';
+
 export const Test = () => {
-  return <div className="bg-red-500">Test</div>;
+  return (
+    <div className="bg-red-500">
+      Test
+      <Button>Click me</Button>
+    </div>
+  );
 };
