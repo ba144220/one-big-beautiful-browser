e3d0de436c19982bc626f62082ae3d993a123d2c
ba144220 <hs41511@gmail.com>
2025-05-24 03:19:49 -0700

    feat: better buttons

--- pages/side-panel/src/components/chat/user-input/index.tsx
+++ pages/side-panel/src/components/chat/user-input/index.tsx
@@ -1,6 +1,7 @@
 import { useSelectedTabs } from '@src/hooks/use-selected-tabs';
 import TabBadge from './tab-badge';
 import { TabSelection } from './tab-selection';
+import { ArrowUpIcon, ImageIcon, SquareIcon } from 'lucide-react';
 export type UserInputProps = {
   onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
   isLoading: boolean;
@@ -10,7 +11,7 @@ export type UserInputProps = {
 export default function UserInput({ onSubmit, isLoading, onStop }: UserInputProps) {
   const { selectedTabs } = useSelectedTabs();
   return (
-    <div className="bg-background rounded-md py-2">
+    <div className="bg-background rounded-md pt-2 pb-1">
       <form onSubmit={onSubmit}>
         <div className="px-2 flex flex-wrap gap-1">
           <TabSelection />
@@ -26,13 +27,26 @@ export default function UserInput({ onSubmit, isLoading, onStop }: UserInputProp
           />
         </div>
         <div className="flex gap-2 px-2 justify-end pt-1 w-full">
+          <button
+            type="button"
+            className="w-5 h-5 p-0 cursor-pointer flex items-center justify-center text-muted-foreground hover:text-foreground transition-all duration-200">
+            <ImageIcon strokeWidth={2} className="size-3" />
+          </button>
+
           {isLoading ? (
-            <button key="stop" type="button" onClick={onStop}>
-              Stop
+            <button
+              type="button"
+              key="stop"
+              onClick={onStop}
+              className="w-5 h-5 p-0 rounded-full cursor-pointer flex items-center justify-center text-primary-foreground bg-primary/80 hover:bg-primary hover:scale-110 transition-all duration-200">
+              <SquareIcon strokeWidth={2} className="size-2.5" />
             </button>
           ) : (
-            <button key="submit" type="submit">
-              Send
+            <button
+              type="submit"
+              key="submit"
+              className="w-5 h-5 p-0 rounded-full cursor-pointer flex items-center justify-center text-primary-foreground bg-primary/80 hover:bg-primary hover:scale-110 transition-all duration-200">
+              <ArrowUpIcon strokeWidth={2} className="size-3" />
             </button>
           )}
         </div>
