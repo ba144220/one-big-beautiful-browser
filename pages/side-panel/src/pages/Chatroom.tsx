ddf5ece683222e4864c49e852efde97b914ef82f
ba144220 <hs41511@gmail.com>
2025-05-23 18:36:03 -0700

    feat: user input ui

--- pages/side-panel/src/pages/Chatroom.tsx
+++ pages/side-panel/src/pages/Chatroom.tsx
@@ -1,9 +1,9 @@
 import { useStream } from '@langchain/langgraph-sdk/react';
 import type { Message } from '@langchain/langgraph-sdk';
 import { MessageContainer } from '../components/chat/message-container';
-import UserPrompt from '../components/chat/user-prompt';
 import useInterrupt from '../hooks/useInterrupt';
 import type { InterruptType } from '@extension/shared';
+import UserInput from '@src/components/chat/user-input';
 
 export default function Chatroom() {
   const thread = useStream<
@@ -20,19 +20,25 @@ export default function Chatroom() {
   useInterrupt(thread);
 
   return (
-    <div className="h-full flex flex-col">
-      <div className="pb-40 px-4 flex flex-col gap-2">
+    <div className="h-screen flex flex-col overflow-hidden">
+      <div className="pb-40 px-4 flex flex-col gap-2 flex-1 overflow-y-scroll pt-4">
         {thread.messages.map(message => (
           <MessageContainer key={message.id} message={message} />
         ))}
       </div>
-
-      <UserPrompt
-        onSubmit={message => thread.submit({ messages: [{ type: 'human', content: message }] })}
-        isLoading={thread.isLoading}
-        onStop={() => thread.stop()}
-        context={['Current file', 'Project context']}
-      />
+      <div className="px-2 pb-2">
+        <UserInput
+          onSubmit={e => {
+            e.preventDefault();
+            const form = e.target as HTMLFormElement;
+            const message = new FormData(form).get('message') as string;
+            form.reset();
+            thread.submit({ messages: [{ type: 'human', content: message }] });
+          }}
+          isLoading={thread.isLoading}
+          onStop={() => thread.stop()}
+        />
+      </div>
     </div>
   );
 }
